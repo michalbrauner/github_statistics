3154f305d13bc343729c735d93f03fdfaf6f8d9e
Michal Brauner <michal.br@gmail.com>
2019-05-15 05:41:13 +0200

    downloading detail of pull request

--- GithubApiClient/github_api_client.py
+++ GithubApiClient/github_api_client.py
@@ -3,9 +3,10 @@ import re
 import ssl
 import sys
 import urllib.request
+import urllib.response
 from http import cookiejar
+from http.client import HTTPResponse
 from typing import List
-
 
 class GithubApiClient(object):
     API_URL = 'https://api.github.com'
@@ -33,15 +34,11 @@ class GithubApiClient(object):
             print('Loading page {}'.format(current_page), end='\r')
             sys.stdout.flush()
 
-            password_manager = urllib.request.HTTPPasswordMgrWithPriorAuth()
-            password_manager.add_password(None, url, self.username, self.token, is_authenticated=True)
-
-            request_opener = self.create_request_opener(password_manager)
-            request = urllib.request.Request(url=url, method='GET')
-            response = request_opener.open(request)
+            response = self.make_request_to_github(url)
 
             links = self.parse_links(response.headers.get('Link'))
-            data_all = data_all + json.loads(response.read())
+            data_for_this_page = self.append_detailed_data_about_pull_requests(json.loads(response.read()))
+            data_all = data_all + data_for_this_page
 
             if 'next' in links:
                 url = links['next']
@@ -51,7 +48,38 @@ class GithubApiClient(object):
 
         return data_all
 
-    def parse_links(self, links_from_header: str) -> dict:
+    def make_request_to_github(self, url: str) -> HTTPResponse:
+        password_manager = urllib.request.HTTPPasswordMgrWithPriorAuth()
+        password_manager.add_password(None, url, self.username, self.token, is_authenticated=True)
+
+        request_opener = self.create_request_opener(password_manager)
+        request = urllib.request.Request(url=url, method='GET')
+
+        return request_opener.open(request)
+
+    def append_detailed_data_about_pull_requests(self, pull_requests_data: List[dict]) -> List[dict]:
+        detailed_pull_requests_data = []
+
+        for pull_request_data in pull_requests_data:
+            pull_request_detail = self.get_detail_data_from_pull_request(pull_request_data)
+
+            pull_request_data_final = pull_request_data
+            pull_request_data_final['detail_info'] = pull_request_detail
+
+            detailed_pull_requests_data.append(pull_request_data_final)
+
+            break
+
+        return detailed_pull_requests_data
+
+    def get_detail_data_from_pull_request(self, pull_request_data: dict) -> dict:
+        response = self.make_request_to_github(pull_request_data['url'])
+        pull_request_detail = json.loads(response.read())
+
+        return pull_request_detail
+
+    @staticmethod
+    def parse_links(links_from_header: str) -> dict:
         links = {}
 
         search_data = re.findall(r'<([^>]*)>; rel=\"([\w]+)\"', links_from_header, re.MULTILINE)
