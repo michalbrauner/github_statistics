6434733e0fe92bf1780e83994ba13aa7ca62a2fd
Michal Brauner <michal.br@gmail.com>
2019-05-16 05:19:34 +0200

    multithreading

--- GithubApiClient/github_api_client.py
+++ GithubApiClient/github_api_client.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import queue
 import re
 import ssl
 import sys
@@ -9,6 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 from http import cookiejar
 from http.client import HTTPResponse
 from typing import List
+
 
 class GithubApiClient(object):
     API_URL = 'https://api.github.com'
@@ -25,35 +27,39 @@ class GithubApiClient(object):
     def token(self):
         return self._token
 
-    def get_pull_requests(self):
+    def get_pull_requests(self, page_start: int, page_end: int) -> List[dict]:
         data_all = []
 
-        url = '{}/repos/BrandEmbassy/platform-backend/pulls?state=all'.format(self.API_URL)
+        url = '{}/repos/BrandEmbassy/platform-backend/pulls?state=all&page={}'.format(self.API_URL, page_start)
 
-        current_page = 1
+        current_page = page_start
 
-        while True:
-            print('Loading page {}'.format(current_page), end='\r')
+        total_pages_number = None
+
+        while current_page <= page_end:
+            if total_pages_number is None:
+                print('Downloading', end='\r')
+            else:
+                print('Downloading page {} / {}'.format(current_page, min(total_pages_number, page_end)), end='\r')
             sys.stdout.flush()
 
             response = self.make_request_to_github(url)
 
             links = self.parse_links(response.headers.get('Link'))
+            if total_pages_number is None and 'last' in links:
+                total_pages_number = links['last']['page']
 
-            data_for_this_page = []
-
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(
-                self.append_detailed_data_about_pull_requests(data_for_this_page, json.loads(response.read())))
-
-            data_all = data_all + data_for_this_page
+            data_for_page = self.append_detailed_data_about_pull_requests(json.loads(response.read()))
+            data_all = data_all + data_for_page
 
             if 'next' in links:
-                url = links['next']
+                url = links['next']['link']
                 current_page = current_page + 1
             else:
                 break
+
+        print('                                                              ')
+        sys.stdout.flush()
 
         return data_all
 
@@ -66,30 +72,36 @@ class GithubApiClient(object):
 
         return request_opener.open(request)
 
-    async def append_detailed_data_about_pull_requests(self, data_for_this_page: List[dict],
-                                                       pull_requests_data: List[dict]) -> None:
+    def append_detailed_data_about_pull_requests(self, pull_requests_data: List[dict]) -> List[dict]:
         detailed_pull_requests_data = []
+
+        pull_requests_data_queue = queue.Queue()
 
         loop = asyncio.get_event_loop()
         futures = []
         executor = ThreadPoolExecutor(max_workers=len(pull_requests_data))
 
         for pull_request_data in pull_requests_data:
-            futures.append(loop.run_in_executor(executor, self.add_detail_data_for_pull_request, pull_request_data,
-                                                detailed_pull_requests_data))
-            break
+            futures.append(loop.run_in_executor(executor, self.finalize_pull_request_data_and_write_to_queue,
+                                                pull_request_data, pull_requests_data_queue))
 
-        done, futures = await asyncio.wait(futures, loop=loop, return_when=asyncio.ALL_COMPLETED)
+        loop.run_until_complete(asyncio.wait(futures, loop=loop, return_when=asyncio.ALL_COMPLETED))
 
-        data_for_this_page = data_for_this_page + detailed_pull_requests_data
+        while True:
+            try:
+                pull_request_data = pull_requests_data_queue.get_nowait()
+                detailed_pull_requests_data.append(pull_request_data)
+            except queue.Empty:
+                break
 
-    def add_detail_data_for_pull_request(self, pull_request_data: dict, detailed_pull_requests_data: List[dict]):
-        pull_request_detail = self.get_detail_data_from_pull_request(pull_request_data)
+        return detailed_pull_requests_data
 
+    def finalize_pull_request_data_and_write_to_queue(self, pull_request_data: dict,
+                                                      pull_requests_data_queue: queue.Queue) -> None:
         pull_request_data_final = pull_request_data
-        pull_request_data_final['detail_info'] = pull_request_detail
+        pull_request_data_final['detail_info'] = self.get_detail_data_from_pull_request(pull_request_data)
 
-        detailed_pull_requests_data.append(pull_request_data_final)
+        pull_requests_data_queue.put(pull_request_data_final)
 
     def get_detail_data_from_pull_request(self, pull_request_data: dict) -> dict:
         response = self.make_request_to_github(pull_request_data['url'])
@@ -101,10 +113,13 @@ class GithubApiClient(object):
     def parse_links(links_from_header: str) -> dict:
         links = {}
 
-        search_data = re.findall(r'<([^>]*)>; rel=\"([\w]+)\"', links_from_header, re.MULTILINE)
+        search_data = re.findall(r'<([^>]*(page=([0-9]+))[^>]*)>; rel=\"([\w]+)\"', links_from_header, re.MULTILINE)
 
         for search_data_item in search_data:
-            links[search_data_item[1]] = search_data_item[0]
+            links[search_data_item[3]] = {
+                'link': search_data_item[0],
+                'page': int(search_data_item[2]),
+            }
 
         return links
 
--- download_data_from_github.py
+++ download_data_from_github.py
@@ -10,6 +10,8 @@ def get_argument_parser() -> argparse.ArgumentParser:
     parser.add_argument('-r', '--repository', type=str, required=True)
     parser.add_argument('-u', '--github-username', type=str, required=True)
     parser.add_argument('-t', '--github-token', type=str, required=True)
+    parser.add_argument('-s', '--page-start', type=int, required=False, default=1)
+    parser.add_argument('-e', '--page-end', type=int, required=False, default=99999)
 
     return parser
 
@@ -19,12 +21,12 @@ def main() -> None:
     arguments = argument_parser.parse_args()
 
     client = GithubApiClient(arguments.github_username, arguments.github_token)
-    pull_requests = client.get_pull_requests()
+    pull_requests = client.get_pull_requests(arguments.page_start, arguments.page_end)
 
     with open(arguments.output, 'w') as output_file:
         json.dump(pull_requests, output_file)
 
-    print('Data saved.        ')
+    print('Data saved in {}'.format(arguments.output))
 
 
 if __name__ == "__main__":
